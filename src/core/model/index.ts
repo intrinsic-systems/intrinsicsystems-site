4e241e8ae2d53c5e368f3640ded9f1136688f2b1
m0nk1wrench <stew.duncan@gmail.com>
2026-05-20 11:43:50 +1000

    Establish canonical CORE model structure

---
+++ src/core/model/index.ts
@@ -0,0 +1,12 @@
+export * from "./coreModel";
+export * from "./coreEvidence";
+export * from "./coreConfidence";
+export * from "./coreTriggers";
+
+export * from "./domains";
+export * from "./elements";
+export * from "./capabilities";
+export * from "./probes";
+export * from "./evidence";
+export * from "./triggers";
+export * from "./scoring";
