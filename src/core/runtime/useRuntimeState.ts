c5d7718af455db471fc9e66f8ec38b00f7df607e
m0nk1wrench <stew.duncan@gmail.com>
2026-05-25 12:13:46 +1000

    Add React runtime state hook

---
+++ src/core/runtime/useRuntimeState.ts
@@ -0,0 +1,52 @@
+import { useMemo, useState } from "react";
+
+import {
+  applyRuntimeMutation,
+  type EnterpriseRuntimeState,
+  type RuntimeAnswerMutation,
+} from "./runtimeEngine";
+
+export function useRuntimeState() {
+  const [runtime, setRuntime] =
+    useState<EnterpriseRuntimeState>({
+      capabilities: {},
+    });
+
+  function mutate(
+    mutation: RuntimeAnswerMutation,
+  ) {
+    setRuntime((current) =>
+      applyRuntimeMutation(
+        current,
+        mutation,
+      ),
+    );
+  }
+
+  const capabilityList = useMemo(() => {
+    return Object.values(runtime.capabilities);
+  }, [runtime]);
+
+  const enterpriseScore = useMemo(() => {
+    if (capabilityList.length === 0) {
+      return 0;
+    }
+
+    return (
+      capabilityList.reduce(
+        (sum, item) => sum + item.score,
+        0,
+      ) / capabilityList.length
+    );
+  }, [capabilityList]);
+
+  return {
+    runtime,
+
+    capabilityList,
+
+    enterpriseScore,
+
+    mutate,
+  };
+}
